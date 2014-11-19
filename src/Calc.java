f02b1d93bc4eda054fe52123be0b3033ecae9902
dc791164 <dc791164@C39373.ens.uqam.ca>
2014-11-19 14:24:56 -0300

    Fini

--- src/Calc.java
+++ src/Calc.java
@@ -1,5 +1,5 @@
 class Arbre {
-    public int type; // 0 = nombre, 1 = addition, 2 = soustraction, 3= multiplication, 4 = division
+    public int type; // 0 = nombre, 1 = addition, 2 = soustraction, 3= multiplication, 4 = division, 5 = x
     public int val;  // valeur de l'entier si type = 0
     public Arbre filsGauche;
     public Arbre filsDroit;
@@ -19,11 +19,31 @@ public class Calc {
     
     public static void main(String[] args) {
         int total;
-        char caractere = Pep8.chari();
+        int valeurX = 1;
         Arbre racine = new Arbre();
         Arbre courant = racine;
-
-        total = (int) gestionCaractere(caractere);
+        constructionBranche(courant);
+        
+        char caractere = Pep8.chari();
+        
+        while (caractere != '.') {
+            if (caractere == '=') {
+                total = calculerTotal(racine,valeurX);
+                Pep8.deco(total);
+                Pep8.charo('\n');
+            }
+            else if (caractere == '>') {
+                afficherInfixe(racine);
+                Pep8.charo('\n');
+            }
+            else if (caractere == 'x') {
+                valeurX = Pep8.deci();
+            }
+            caractere = Pep8.chari();
+        }
+        Pep8.charo('.');
+        Pep8.charo('\n');
+        /*
         caractere = Pep8.chari();
         while (caractere != '.') {
             if (caractere == '=') {
@@ -34,53 +54,111 @@ public class Calc {
         }
         Pep8.charo('.');
         Pep8.charo('\n');
+                
+        */
         
     }
     
     public static void constructionBranche(Arbre courant) {
         char caractere = Pep8.chari();
-        if (caractere == '+' || caractere == '-' ||caractere == '*' ||caractere == '/') {            
-            courant.val = caractere;
+        if (caractere == '+' || caractere == '-' ||caractere == '*' ||caractere == '/') {     
+            switch(caractere) {
+                case '+':
+                    courant.type = 1;
+                    break;
+                case '-':
+                    courant.type = 2;
+                    break;
+                case '*':
+                    courant.type = 3;
+                    break;
+                case '/':
+                    courant.type = 4;
+                    break;
+            }
             courant.filsGauche = new Arbre();
             constructionBranche(courant.filsGauche);
             courant.filsDroit = new Arbre();
             constructionBranche(courant.filsDroit);            
         }
-        else if (caractere > '0' && caractere < '9') {
-            courant.val = caractere;
+        else if (caractere >= '0' && caractere <= '9') {
+            courant.type = 0;
+            courant.val = lectureNombre(conversionCharToInt(caractere));
+        }
+        else if (caractere == 'x') {
+            courant.type = 5;
         }
         else if (caractere == ' ' || caractere == '\n') {
             constructionBranche(courant);
         }
     }
     
-    public static int gestionCaractere(char caractere) {
-        if (caractere >= '0' && caractere<= '9') {
-            //return lectureNombre(conversionCharToInt(caractere));
+  
+    
+    public static void afficherInfixe(Arbre courant) {
+        if (courant.type == 0) {
+           Pep8.deco(courant.val); 
         }
-        else if (caractere == '+' ) {
-            return (addition());
+        else if (courant.type == 5) {
+            Pep8.charo('x');
         }
-        else if (caractere == '-' ) {
-            return (soustraction());
+        else {
+            Pep8.charo('(');
+            afficherInfixe(courant.filsGauche);
+            switch(courant.type) {
+                case 1:
+                    Pep8.charo('+');
+                    break;
+                case 2:
+                    Pep8.charo('-');
+                    break;
+                case 3:
+                    Pep8.charo('*');
+                    break;
+                case 4:
+                    Pep8.charo('/');
+                    break;
+            }
+            afficherInfixe(courant.filsDroit);
+            Pep8.charo(')');
         }
-        else if (caractere == '*' ) {
-            return (multiplication());
-        }
-        else if (caractere == '/' ) {
-            return (division());
-        }
-        else if (caractere == ' ' || caractere == '\n')
-        {
-            return gestionCaractere(Pep8.chari());
-        }
-        return 0;
+        
     }
     
-    public static char lectureNombre(char nombre) {
+    public static int calculerTotal(Arbre courant, int valeurX) {
+        int valeur;
+        if (courant.type == 0) {
+            valeur = courant.val;
+        }
+        else if (courant.type == 5) {
+            valeur = valeurX;
+        }
+        else {
+            switch(courant.type) {
+                case 1:
+                    valeur = addition(courant, valeurX);
+                    break;
+                case 2:
+                    valeur = soustraction(courant, valeurX);
+                    break;
+                case 3:
+                    valeur = multiplication(courant, valeurX);
+                    break;
+                case 4:
+                    valeur = division(courant, valeurX);
+                    break;
+                default:
+                    valeur = 0;
+            }
+        }
+        return valeur;
+    }
+            
+    
+    public static int lectureNombre(int nombre) {
         char nouveauNombre = Pep8.chari();
         if (nouveauNombre >= '0' && nouveauNombre <= '9') {
-            return lectureNombre(conversionIntToChar(conversionCharToInt(nombre)*10 + conversionCharToInt(nouveauNombre)));
+            return lectureNombre(nombre*10 + conversionCharToInt(nouveauNombre));
         }
         else {
             return nombre;
@@ -88,60 +166,27 @@ public class Calc {
  
     }
     
-    public static int addition() {
-        int premierElement;
-        int deuxiemeElement;
-        char temp;
-        temp = Pep8.chari();
-        premierElement = gestionCaractere(temp);
-        temp = Pep8.chari();
-        deuxiemeElement = gestionCaractere(temp);
-        return (premierElement + deuxiemeElement);
+    public static int addition(Arbre courant, int valeurX) {
+        return calculerTotal(courant.filsGauche, valeurX) + calculerTotal(courant.filsDroit, valeurX);
         
     }
     
-    public static int multiplication() {
-        int premierElement;
-        int deuxiemeElement;
-        char temp;
-        temp = Pep8.chari();
-        premierElement = gestionCaractere(temp);
-        temp = Pep8.chari();
-        deuxiemeElement = gestionCaractere(temp);
-        return (premierElement * deuxiemeElement);
+    public static int multiplication(Arbre courant, int valeurX) {
+        return calculerTotal(courant.filsGauche, valeurX) * calculerTotal(courant.filsDroit, valeurX);
     }
     
-    public static int division() {
-        int premierElement;
-        int deuxiemeElement;
-        char temp;
-        temp = Pep8.chari();
-        premierElement = gestionCaractere(temp);
-        temp = Pep8.chari();
-        deuxiemeElement = gestionCaractere(temp);
-        return (premierElement / deuxiemeElement);
+    public static int division(Arbre courant, int valeurX) {
+        return calculerTotal(courant.filsGauche, valeurX) / calculerTotal(courant.filsDroit, valeurX);
     }
     
-    public static int soustraction() {
-        int premierElement;
-        int deuxiemeElement;
-        char temp;
-        temp = Pep8.chari();
-        premierElement = gestionCaractere(
-                
-                
-        temp = Pep8.chari();
-        deuxiemeElement = gestionCaractere(temp);
-        return (premierElement - deuxiemeElement);
+    public static int soustraction(Arbre courant, int valeurX) {
+        return calculerTotal(courant.filsGauche, valeurX) - calculerTotal(courant.filsDroit, valeurX);
     }
     
     public static int conversionCharToInt(char caractere)
     {
         return ((int) caractere) - 48;
     }
-    public static char conversionIntToChar(int nombre){
-        return 
-    }
-            
+       
     
 }
