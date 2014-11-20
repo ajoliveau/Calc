550a7b53d5d725046275fcb95f2337e61d7e893a
dc791164 <dc791164@C32026.ens.uqam.ca>
2014-11-20 14:39:52 -0300

    Modification du nom de la classe

--- src/Calc.java
+++ src/Calc.java
@@ -7,6 +7,7 @@
 * calculer le résultat de cette expression, et de l'afficher en notation infixe 
 *
 * Paramètres :
+* 
 * - L'expression a évaluer, constituée de caractéres
 * - Une ou plusieurs commandes :
 *       - La commande 'x' suivie d'un nombre qui assigne ce nombre a la valeur de x
@@ -16,6 +17,31 @@
 * 
 * Fonctionnement :
 * 
+* L'expression à évaluer est lue caractère par caractère. Le programme fait la différence
+* entre les opérateurs (+, - , * , / et x) et les nombres.
+* Si le caractère est un chiffre, il lit tous les caractères suivant jusqu'a 
+* tomber sur autre chose qu'un chiffre, et reconstitue le nombre complet.
+* 
+* L'expression est stockée dans un arbre. L'arbre est une succession de branches
+* qui contiennent chacune un opérateur ou un nombre. Si la branche contient un 
+* opérateur de calcul, elle possède également un  lien vers deux autres branches
+* qui représentent les paramètres de cet opérateur.
+* 
+* Une fois l'expression stockée dans l'arbre, le programme reconnait plusieurs
+* commandes qui vont parcourir récursivement toutes les branches de l'arbre pour
+* effectuer les opérations demandées.
+* 
+* Limites :
+* 
+* - Le programme ne prend en charge que les nombres entiers. L'opérateur division
+* est donc une division entière
+* - Le programme ne gère pas toutes les erreurs utilisateurs possibles lors de
+* l'entrée de l'expression
+*
+* 
+* Autoévaluation :
+* 
+* Le programme passe tous les tests
 *
 *
 *
@@ -24,7 +50,9 @@
 */
 
 
-
+/*
+* Structure de donnée permettant de stocker l'expression
+*/
 class Branche {
     public int type; // 0 = nombre, 1 = addition, 2 = soustraction, 3= multiplication, 4 = division, 5 = x
     public int val;  // valeur de l'entier si type = 0
@@ -45,18 +73,18 @@ public class Calc {
      
     
     public static void main(String[] args) {
-        int total;
-        int valeurX = 1;
+        int valeurX = 1; //valeur par défaut affectée a x s'il n'est pas défini
+        
         Branche racine = new Branche(); //reference vers l'arbre au complet
         Branche courant = racine;
         
         constructionArbre(courant); //construit récursivement l'arbre
         
-        char caractere = Pep8.chari();
+        char caractere = Pep8.chari(); 
         
         while (caractere != '.') { //boucle dans les commandes de calcul et d'affichage
             if (caractere == '=') {
-                total = calculerTotal(racine,valeurX);
+                int total = calculerTotal(racine,valeurX);
                 Pep8.deco(total);
                 Pep8.charo('\n');
             }
@@ -70,22 +98,27 @@ public class Calc {
             caractere = Pep8.chari();
         }
         Pep8.charo('.');
-        Pep8.charo('\n');
-        /*
-        caractere = Pep8.chari();
-        while (caractere != '.') {
-            if (caractere == '=') {
-                Pep8.deco(total);
-                Pep8.charo('\n');
-            }
-            caractere = Pep8.chari();
-        }
-        Pep8.charo('.');
-        Pep8.charo('\n');
-                
-        */
-        
+        Pep8.charo('\n');      
     }
+    
+    /**
+     * 
+     * Construit la branche passée en paramètre  :
+     *      - Si le caractère est un chiffre, lis le nombre en entier et le 
+     * stocke dans la branche.
+     *      - Si le caractère est un 'x', le stocke dans la branche.
+     *      - Si le caractère est un opérateur, le stocke dans la branche, puis
+     * construis les branches inférieures gauches, puis droites.
+     *      - Si le caractère est un espace ou un retour a la ligne, ignore
+     * le caractère
+     * 
+     * Paramètre : 
+     *      - Branche courant : branche de l'arbre à traiter
+     * 
+     * Effet de bord : 
+     *      Aucun
+     * 
+     */
     
     public static void constructionArbre(Branche courant) {
         char caractere = Pep8.chari();
@@ -104,24 +137,47 @@ public class Calc {
                     courant.type = 4;
                     break;
             }
-            courant.filsGauche = new Branche();
-            constructionArbre(courant.filsGauche);
-            courant.filsDroit = new Branche();
-            constructionArbre(courant.filsDroit);            
+            courant.filsGauche = new Branche(); // Un opérateur a forcément deux
+            constructionArbre(courant.filsGauche);// opérandes. Il faut construire
+            courant.filsDroit = new Branche(); // une branche supplémentaire de
+            constructionArbre(courant.filsDroit);// l'arbre pour chacun.            
         }
-        else if (caractere >= '0' && caractere <= '9') {
+        else if (caractere >= '0' && caractere <= '9') { 
             courant.type = 0;
             courant.val = lectureNombre(conversionCharToInt(caractere));
+            // si le caractère est un chiffre, il faut vérifier si c'est un nombre
+            
         }
         else if (caractere == 'x') {
             courant.type = 5;
         }
         else if (caractere == ' ' || caractere == '\n') {
-            constructionArbre(courant);
+            constructionArbre(courant); // on saute le caractère
+        }
+        else {
+            Pep8.stro("L'expression est incorrecte\n");
+            Pep8.stop();
         }
     }
     
   
+    /**
+     * 
+     * Affiche l'expression contenue dans la branche et ses filles en 
+     * notation infixe :
+     *      - Si le noeud est un chiffre, écrit sa valeur
+     *      - Si le noeud est un 'x', écrit le caractère.
+     *      - Si le noeud est un opérateur, mets entre parenthèses le contenu
+     * de l'expression dans les branches inférieures séparé par l'opérateur
+     *      
+     * 
+     * Paramètre : 
+     *      - Branche courant : branche de l'arbre à traiter
+     * 
+     * Effet de bord : 
+     *      Ecrit dans la console
+     * 
+     */
     
     public static void afficherInfixe(Branche courant) {
         if (courant.type == 0) {
@@ -130,8 +186,9 @@ public class Calc {
         else if (courant.type == 5) {
             Pep8.charo('x');
         }
-        else {
-            Pep8.charo('(');
+        else { // si la branche représente un opérateur, on affiche sous la forme
+               // "(valGauche[opérateur]valDroite)"
+            Pep8.charo('('); 
             afficherInfixe(courant.filsGauche);
             switch(courant.type) {
                 case 1:
@@ -153,6 +210,28 @@ public class Calc {
         
     }
     
+    /**
+     * 
+     * Calcule la valeur de l'expression contenue dans la branche courante
+     * et les branches inférieures :
+     *      - Si le noeud est un chiffre, renvoie sa valeur
+     *      - Si le noeud est un 'x', écrit la valeur enregistrée de x.
+     *      - Si le noeud est un opérateur, renvoie le résultat de l'opération
+     * entre le total dans la branche droite et le total dans la branche gauche
+     *      
+     * 
+     * Paramètre : 
+     *      - Branche courant : branche de l'arbre à traiter
+     *      - int valeurX : valeur enregistrée pour x
+     * 
+     * Retour :
+     *      - int total : valeur de l'expression évaluée de la branche
+     * 
+     * Effet de bord : 
+     *      Aucun
+     * 
+     */
+    
     public static int calculerTotal(Branche courant, int valeurX) {
         int valeur;
         if (courant.type == 0) {
@@ -162,7 +241,7 @@ public class Calc {
             valeur = valeurX;
         }
         else {
-            switch(courant.type) {
+            switch(courant.type) { 
                 case 1:
                     valeur = addition(courant, valeurX);
                     break;
@@ -182,38 +261,137 @@ public class Calc {
         return valeur;
     }
             
+    /**
+     * 
+     * Lis l'entrée pour reconstituer un nombre a partir des 
+     * caractères individuels.
+     *      
+     * 
+     * Paramètre : 
+     *      - int nombre : le nombre deja lu et traité
+     * 
+     * Retour :
+     *      - int nombre : la valeur des nombres lus et traités
+     * 
+     * Effet de bord : 
+     *      Aucun
+     * 
+     */
     
     public static int lectureNombre(int nombre) {
         char nouveauNombre = Pep8.chari();
         if (nouveauNombre >= '0' && nouveauNombre <= '9') {
             return lectureNombre(nombre*10 + conversionCharToInt(nouveauNombre));
-        }
+        } // car 195 = (1*10+9)*10 + 5
         else {
             return nombre;
         }
  
     }
     
+    /**
+     * 
+     * Additionne les valeurs évaluées de l'expression dans la branche gauche
+     * et la branche droite de la branche courante.
+     * 
+     * Paramètre : 
+     *      - Branche courant : branche de l'arbre à traiter
+     *      - int valeurX : valeur enregistrée pour x
+     * 
+     * Retour :
+     *      - int nombre : la somme des branches gauches et droites
+     * 
+     * Effet de bord : 
+     *      Aucun
+     * 
+     */
+    
     public static int addition(Branche courant, int valeurX) {
         return calculerTotal(courant.filsGauche, valeurX) + calculerTotal(courant.filsDroit, valeurX);
         
     }
     
+    /**
+     * 
+     * Multiplie les valeurs évaluées de l'expression dans la branche gauche
+     * et la branche droite de la branche courante.
+     * 
+     * Paramètre : 
+     *      - Branche courant : branche de l'arbre à traiter
+     *      - int valeurX : valeur enregistrée pour x
+     * 
+     * Retour :
+     *      - int nombre : le produit des branches gauches et droites
+     * 
+     * Effet de bord : 
+     *      Aucun
+     * 
+     */
+    
     public static int multiplication(Branche courant, int valeurX) {
         return calculerTotal(courant.filsGauche, valeurX) * calculerTotal(courant.filsDroit, valeurX);
     }
+    
+    /**
+     * 
+     * Divise les valeurs évaluées de l'expression dans la branche gauche
+     * et la branche droite de la branche courante.
+     * 
+     * Paramètre : 
+     *      - Branche courant : branche de l'arbre à traiter
+     *      - int valeurX : valeur enregistrée pour x
+     * 
+     * Retour :
+     *      - int nombre : le quotient des branches gauches et droites
+     * 
+     * Effet de bord : 
+     *      Aucun
+     * 
+     */
     
     public static int division(Branche courant, int valeurX) {
         return calculerTotal(courant.filsGauche, valeurX) / calculerTotal(courant.filsDroit, valeurX);
     }
     
+    /**
+     * 
+     * Soustrais les valeurs évaluées de l'expression dans la branche gauche
+     * et la branche droite de la branche courante.
+     * 
+     * Paramètre : 
+     *      - Branche courant : branche de l'arbre à traiter
+     *      - int valeurX : valeur enregistrée pour x
+     * 
+     * Retour :
+     *      - int nombre : la différence des branches gauches et droites
+     * 
+     * Effet de bord : 
+     *      Aucun
+     * 
+     */
+    
     public static int soustraction(Branche courant, int valeurX) {
         return calculerTotal(courant.filsGauche, valeurX) - calculerTotal(courant.filsDroit, valeurX);
     }
     
+    /**
+     * 
+     * Convertit un caractère en chiffre
+     * 
+     * Paramètre : 
+     *      - char caractere : caractere a convertir
+     * 
+     * Sortie :
+     *      - int nombre    : caractere convertit
+     * 
+     * Effets de bord : 
+     *      Aucun 
+
+     */
+    
     public static int conversionCharToInt(char caractere)
     {
-        return ((int) caractere) - 48;
+        return ((int) caractere) - 48; // le code ASCII de '0' est 48
     }
        
     
